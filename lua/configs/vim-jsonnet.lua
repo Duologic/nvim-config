84a85b0f312e0a04d85f635a2568ed4bfcaa068e
Duologic <jeroen@simplistic.be>
2023-05-23 11:17:33 +0200

    fix lspconfig

--- lua/configs/vim-jsonnet.lua
+++ lua/configs/vim-jsonnet.lua
@@ -53,7 +53,7 @@ vim.api.nvim_create_autocmd(
             vim.keymap.set('n', '<leader>j', '<cmd>JsonnetEval<cr>')
             vim.keymap.set('n', '<leader>k', '<cmd>JsonnetEvalString<cr>')
 
-            if os.getenv('JSONNET_BIN') ~= '' then
+            if os.getenv('JSONNET_BIN') ~= nil and os.getenv('JSONNET_BIN') ~= '' then
                 vim.g.jsonnet_command = os.getenv('JSONNET_BIN') .. ' -J . -J vendor -J lib'
                 vim.g.jsonnet_string_command = os.getenv('JSONNET_BIN') .. ' -S -J . -J vendor -J lib'
             else
