d26cf36f216dc565acd744ca70f68043f9702405
Duologic <jeroen@simplistic.be>
2023-05-23 09:41:56 +0200

    fixes

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -58,7 +58,7 @@ vim.api.nvim_create_autocmd(
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
